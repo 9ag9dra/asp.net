c9b138ad669265478e64c92796b926aed172fed6
9ag9dra <133749883+9ag9dra@users.noreply.github.com>
2023-05-16 13:20:34 +0530

    Add files via upload

---
+++ 01.Calculator application win form/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace _01.Calculator_application_win_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.subtractbutton = new System.Windows.Forms.Button();
+            this.multiplybutton = new System.Windows.Forms.Button();
+            this.dividebutton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(409, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MY CALCULATOR APPLICATION ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label2.Location = new System.Drawing.Point(91, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label3.Location = new System.Drawing.Point(91, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(294, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ENTER SECOND NUMBER";
+            // 
+            // addbutton
+            // 
+            this.addbutton.BackColor = System.Drawing.Color.Yellow;
+            this.addbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addbutton.Location = new System.Drawing.Point(82, 210);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(135, 43);
+            this.addbutton.TabIndex = 3;
+            this.addbutton.Text = "ADDITION";
+            this.addbutton.UseVisualStyleBackColor = false;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // subtractbutton
+            // 
+            this.subtractbutton.BackColor = System.Drawing.Color.Yellow;
+            this.subtractbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtractbutton.Location = new System.Drawing.Point(223, 209);
+            this.subtractbutton.Name = "subtractbutton";
+            this.subtractbutton.Size = new System.Drawing.Size(182, 44);
+            this.subtractbutton.TabIndex = 4;
+            this.subtractbutton.Text = "SUBTRACTION";
+            this.subtractbutton.UseVisualStyleBackColor = false;
+            this.subtractbutton.Click += new System.EventHandler(this.subtractbutton_Click);
+            // 
+            // multiplybutton
+            // 
+            this.multiplybutton.BackColor = System.Drawing.Color.Yellow;
+            this.multiplybutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplybutton.Location = new System.Drawing.Point(426, 207);
+            this.multiplybutton.Name = "multiplybutton";
+            this.multiplybutton.Size = new System.Drawing.Size(209, 44);
+            this.multiplybutton.TabIndex = 5;
+            this.multiplybutton.Text = "MULTIPLICATION";
+            this.multiplybutton.UseVisualStyleBackColor = false;
+            this.multiplybutton.Click += new System.EventHandler(this.multiplybutton_Click);
+            // 
+            // dividebutton
+            // 
+            this.dividebutton.BackColor = System.Drawing.Color.Yellow;
+            this.dividebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dividebutton.Location = new System.Drawing.Point(641, 207);
+            this.dividebutton.Name = "dividebutton";
+            this.dividebutton.Size = new System.Drawing.Size(147, 44);
+            this.dividebutton.TabIndex = 6;
+            this.dividebutton.Text = "DIVISION";
+            this.dividebutton.UseVisualStyleBackColor = false;
+            this.dividebutton.Click += new System.EventHandler(this.dividebutton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(407, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 25);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(407, 131);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 25);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(146, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 44);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "lable4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Magenta;
+            this.label5.Location = new System.Drawing.Point(92, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(237, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ENTER FIRST NUMBER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(806, 404);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dividebutton);
+            this.Controls.Add(this.multiplybutton);
+            this.Controls.Add(this.subtractbutton);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Button subtractbutton;
+        private System.Windows.Forms.Button multiplybutton;
+        private System.Windows.Forms.Button dividebutton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 01.Calculator application win form/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01.Calculator_application_win_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")                 //testbox1 ke ander value empty nahi de sakte or na hi textbox2 me 
+            {
+
+
+                    float num1 = float.Parse(textBox1.Text);
+                    float num2 = float.Parse(textBox2.Text);
+
+                    float result = num1 + num2;
+                    label4.Text = "addition result is" + " " + result.ToString();   // lebel 4 ke text ko hum result ki value provode kar rahe hai
+                    label4.Visible = true;                                           // lebel 4 ko visible kar liya hai taki hum backend per isko use kar sake 
+
+                    //  MessageBox.Show( "addition result is"+" "+ result.ToString());
+
+                   
+                
+  
+            }
+            else
+            {
+                MessageBox.Show("please enter valid  number ");
+            }
+
+        }
+
+        private void subtractbutton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != ""  && textBox2.Text!= "")   // textbox1 and textbox2 ki  value empty n ahi hiniu chahiye
+            {
+
+
+                float num1 = float.Parse(textBox1.Text);
+                float num2 = float.Parse(textBox2.Text);
+
+                float result = num1 - num2;
+                label4.Text = "subtraction result is" + " " + result.ToString();   // lebel 4.text ko hum result ki value provide kar rahe hai 
+                label4.Visible = true;                                             // lebel 4 is visible so that we can see the result given ti it , iski help se hi result show hoga 
+
+                //  MessageBox.Show( "subtraction result is"+" "+ result.ToString());
+            }
+            else
+            {
+                MessageBox.Show("please enter valid  number");
+            }
+
+
+
+        }
+
+        private void multiplybutton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")   // textbox1 and textbox2 ki  value empty n ahi hiniu chahiye
+            {
+
+
+                float num1 = float.Parse(textBox1.Text);
+                float num2 = float.Parse(textBox2.Text);
+
+                float result = num1 * num2;
+                label4.Text = "multiplication result is" + " " + result.ToString();   // lebel 4.text ko hum result ki value provide kar rahe hai 
+                label4.Visible = true;                                             // lebel 4 is visible so that we can see the result given ti it , iski help se hi result show hoga 
+
+                //  MessageBox.Show( "multiplication result is"+" "+ result.ToString());
+            }
+            else
+            {
+                MessageBox.Show("please enter valid  number");
+            }
+
+
+
+        }
+
+        private void dividebutton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox2.Text  != "0")   // textbox1 and textbox2 ki  value empty n ahi hiniu chahiye
+            {
+
+
+                float num1 = float.Parse(textBox1.Text);
+                float num2 = float.Parse(textBox2.Text);
+
+                float result = num1 /num2;
+                label4.Text = "devision result is " + "" + result.ToString();       // lable4 ke ander hum result ki value print karva rahehai 
+                label4.Visible = true;                                             // lebel 4 is visible so that we can see the result given ti it , iski help se hi result show hoga 
+
+                //  MessageBox.Show( "division result is"+" "+ result.ToString());   // message ko messagebix ke throgh bhi show ya print karva sakte hai
+            }
+            else if  ( textBox1.Text != "0" &&  textBox2.Text == "0" )
+            {
+                label4.Text = "infinite";   
+                label4.Visible = true;
+
+              //  MessageBox.Show("infinite");
+
+
+            }
+            else            // if (textBox1.Text ==  "0"   && textBox2.Text  == "0")
+            {
+                label4.Text = "undefined"; 
+                label4.Visible = true;
+              //  MessageBox.Show("undefined");
+            }
+
+            
+        }
+    }
+        
+}  
